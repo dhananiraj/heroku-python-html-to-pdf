5d9dfa72eaff5993a7f249c5c8c4e415ffbee409
= <dhananirajr@gmail.com>
2020-08-29 01:47:01 +0530

    test-1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import *
 from flask_cors import CORS
+from pdfkit import *
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home_view():
-    return "This is the api made by Raj Dhanani and Shubham Bhanderi from RS Tech"
+    from_string("This is the api made by Raj Dhanani and Shubham Bhanderi from RS Tech", 'temp.pdf')
+    return send_file("temp.pdf")
 
 
