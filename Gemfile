0d911b3b0289a7766ee637f951930b54e5b5a310
= <dhananirajr@gmail.com>
2020-08-29 00:49:23 +0530

    added Gemfile

---
+++ Gemfile
@@ -0,0 +1,1 @@
+gem 'wkhtmltopdf-heroku', '2.12.5.0'
--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+    return "This is the api made by Raj Dhanani and Shubham Bhanderi from RS Tech"
+
+
