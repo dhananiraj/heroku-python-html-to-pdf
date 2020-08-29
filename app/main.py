5c6d2e3b77e916f23b35568b0a3ecd3c1f65c6af
= <dhananirajr@gmail.com>
2020-08-29 13:02:33 +0530

    replace string and remove comma and undefined

--- app/main.py
+++ app/main.py
@@ -10,12 +10,17 @@ CORS(app)
 path_wkhtmltopdf = r'/app/bin/wkhtmltopdf'
 
 config = configuration(wkhtmltopdf=path_wkhtmltopdf,options = {
-    'page-size': 'A4',
+    'page-size': 'Letter',
     'margin-top': '0.75in',
-    'margin-right': '0.1in',
+    'margin-right': '0.3in',
     'margin-bottom': '0.75in',
-    'margin-left': '0.1in',
+    'margin-left': '0.3in',
     'encoding': "UTF-8",
+    'custom-header' : [('Accept-Encoding', 'gzip')],
+    'cookie': [
+        ('cookie-name1', 'cookie-value1'),
+        ('cookie-name2', 'cookie-value2'),
+    ],
     'no-outline': None
 }
 )
